f7ee06779873e74f7c2054bd7cb9d6506912f983
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-03-16 23:15:27 -0300

    css dos campos de input e tasks

--- todo-list-app/src/body/Body.js
+++ todo-list-app/src/body/Body.js
@@ -17,23 +17,53 @@ const Body = () => {
                             <img src={iconMoon} alt=""/>
                         </div>
                         <div className="content-body">
-                            <div className="item-input-li">
-                                <div className="check">check</div>
-                                <input type="text" placeholder='type any todo'/>
+                            <div className="main-input">
+                                <div className="item-input-li">
+                                    <label className='custom-radio'>
+                                        <input className='radio-button' type="radio" name="example" value="option1"/>
+                                    </label>
+                                    <input className='input-cool' type="text" placeholder='type any todo'/>
+                                </div>
+                            </div>
+                            <div className="secondary-tasks">
+                                <div className="item-input-li">
+                                    <label className='custom-radio'>
+                                        <input className='radio-button' type="radio" name="example" value="option1"/>
+                                    </label>
+                                    <p>some task todo</p>
+                                </div>
+                                <div className="item-input-li">
+                                    <label className='custom-radio'>
+                                        <input className='radio-button' type="radio" name="example" value="option1"/>
+                                    </label>
+                                    <p>some task todo</p>
+                                </div>
+                                <div className="item-input-li">
+                                    <label className='custom-radio'>
+                                        <input className='radio-button' type="radio" name="example" value="option1"/>
+                                    </label>
+                                    <p>some task todo</p>
+                                </div>
+                            </div>
+                            <div className="item-input-li status">
+                                <div className="items-left">
+                                    <p>5 items left</p>
+                                </div>
+                                <div className="items-main">
+                                    <p>All</p>
+                                    <p>Active</p>
+                                    <p>Completed</p>
+                                </div>
+                                <div className="clear">
+                                    <p>Clear Completed</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="status">
-                            <h3>All</h3>
-                            <h3>Active</h3>
-                            <h3>Completed</h3>
-                        </div>
-                        <div>
+                        <div className='footer'>
                             <p>Drag and drop to reoder list</p>
                         </div>
                     </div>
-                    
                 </div>
-               
             </section>
         </main>
     )
