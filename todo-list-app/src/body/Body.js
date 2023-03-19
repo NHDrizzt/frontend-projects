d6def3886c34ad16eb71c48aa36e94525250da23
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-03-19 15:00:20 -0300

    feature: checkbox

--- todo-list-app/src/body/Body.js
+++ todo-list-app/src/body/Body.js
@@ -4,16 +4,24 @@ import backgroundImageLightThemeMobile from '../images/bg-mobile-light.jpg'
 import backgroundImageDarkTheme from '../images/bg-desktop-dark.jpg'
 import iconSun from '../images/icon-sun.svg'
 import iconMoon from '../images/icon-moon.svg'
+import iconChecked from '../images/icon-check.svg'
+import iconCross from '../images/icon-cross.svg'
 import {useState} from "react";
 
 
 const Body = () => {
     const [isDarkMode, setIsDarkMode] = useState(false)
-    
+    const [isChecked, setIsChecked] = useState(false)
     const toggleDarkMode = () => {
         setIsDarkMode(!isDarkMode)
         !isDarkMode ? document.body.style.backgroundColor = '#171823' : document.body.style.backgroundColor = '#FFF'
     }
+    
+    const toggleCheckmark = () => {
+        setIsChecked(!isChecked)
+    }
+    
+    const classes = `check ${isChecked ? 'check active-check' : 'check'}`;
     
     const imageBackgroundChange = {
         backgroundImage: !isDarkMode ? `url(${backgroundImageLightThemeDesktop})` : `url(${backgroundImageDarkTheme})`,
@@ -26,50 +34,24 @@ const Body = () => {
                     <div className="content-container">
                         <div className="content-header">
                             <h1>TODO</h1>
-                            <img onClick={toggleDarkMode} src={isDarkMode ? iconMoon : iconSun} alt=""/>
+                            <img onClick={toggleDarkMode} src={isDarkMode ? iconSun : iconMoon} alt=""/>
                         </div>
                         <div className="content-body">
                             <div className="main-input">
-                                <div className="item-input-li">
-                                    <label className='custom-radio'>
-                                        <input className='radio-button' type="radio" name="example" value="option1"/>
-                                    </label>
+                                <div className={!isDarkMode ? 'item-input-li-lightmode' : 'item-input-li-darkmode'}>
                                     <input className='input-cool' type="text" placeholder='type any todo'/>
                                 </div>
                             </div>
-                            <div className="secondary-tasks">
-                                <div className="item-input-li">
-                                    <label className='custom-radio'>
-                                        <input className='radio-button' type="radio" name="example" value="option1"/>
-                                    </label>
-                                    <p>some task todo</p>
-                                </div>
-                                <div className="item-input-li">
-                                    <label className='custom-radio'>
-                                        <input className='radio-button' type="radio" name="example" value="option1"/>
-                                    </label>
-                                    <p>some task todo</p>
-                                </div>
-                                <div className="item-input-li">
-                                    <label className='custom-radio'>
-                                        <input className='radio-button' type="radio" name="example" value="option1"/>
-                                    </label>
-                                    <p>some task todo</p>
-                                </div>
-                                <div className="item-input-li">
-                                    <label className='custom-radio'>
-                                        <input className='radio-button' type="radio" name="example" value="option1"/>
-                                    </label>
-                                    <p>some task todo</p>
-                                </div>
-                                <div className="item-input-li">
-                                    <label className='custom-radio'>
-                                        <input className='radio-button' type="radio" name="example" value="option1"/>
-                                    </label>
-                                    <p>some task todo</p>
+                            <div className={!isDarkMode ? 'secondary-tasks secondary-tasks-darkmode' : 'secondary-tasks secondary-tasks-lightmode'}>
+                                <div className={!isDarkMode ? 'item-input-li-lightmode' : 'item-input-li-darkmode'}>
+                                    <div className={classes} onClick={toggleCheckmark}> <img src={iconChecked} alt=""/></div>
+                                    <div className="task-container">
+                                        <p>some task todo</p>
+                                        <button className='close'><img src={iconCross} alt=""/></button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="item-input-li status">
+                            <div className={!isDarkMode ? 'item-input-li-lightmode status' : 'item-input-li-darkmode status'}>
                                 <div className="items-left">
                                     <p>5 items left</p>
                                 </div>
