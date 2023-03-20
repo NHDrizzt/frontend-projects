fa9a6aa3d975744ea1317e3becca567a78308024
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-03-20 17:54:39 -0300

    refatorando o codigo

--- casa-de-cambio-app/src/main.js
+++ casa-de-cambio-app/src/main.js
@@ -6,15 +6,38 @@ const mainDiv = document.querySelector('.content-card-container')
 const pesquisa = document.querySelector('.pesquisar')
 const mainTitle = document.querySelector('.main-title')
 const URL = 'https://api.exchangerate.host/latest?base='
-mainButton.addEventListener('change', () => {
-    console.log(mainButton.value)
-})
+
+const createMainTitle = () => {
+    mainTitle.innerHTML = ''
+    const mainH2 = document.createElement('h2')
+    mainH2.innerHTML = `Valores referentes a 1 ${mainButton.value}`
+    mainTitle.appendChild(mainH2)
+}
+
+const createCards = (data) => {
+    data.forEach(([key, value]) => {
+        const myEle = document.createElement('div')
+        const firstImg = document.createElement('img')
+        const firstP = document.createElement('p')
+        const secondP = document.createElement('p')
+        myEle.classList.add('card-api')
+        firstImg.src = 'coins-svgrepo-com%201.png'
+        firstP.innerHTML = key
+        secondP.innerHTML = value
+        secondP.style.color = '#FFE713';
+        myEle.appendChild(firstImg)
+        myEle.appendChild(firstP)
+        myEle.appendChild(secondP)
+        mainDiv.appendChild(myEle)
+    })
+}
 
 pesquisa.addEventListener('click', () => {
     mainDiv.innerHTML = ''
     fetch(`${URL}${mainButton.value}`)
         .then((response) => response.json())
         .then((data) => {
+            const keyValues = Object.entries(data.rates)
             const myKeys = Object.keys(data.rates)
             if(!myKeys.includes(mainButton.value)) {
                 Swal.fire({
@@ -24,25 +47,8 @@ pesquisa.addEventListener('click', () => {
                     footer: '<a href="">Why do I have this issue?</a>'
                 })
             } else {
-                const myValues = Object.values(data.rates)
-                const mainH2 = document.createElement('h2')
-                mainH2.innerHTML = `Valores referentes a 1 ${mainButton.value}`
-                mainTitle.appendChild(mainH2)
-                myKeys.forEach((item, index) => {
-                    const myEle = document.createElement('div')
-                    const firstImg = document.createElement('img')
-                    const firstP = document.createElement('p')
-                    const secondP = document.createElement('p')
-                    myEle.classList.add('card-api')
-                    firstImg.src = 'coins-svgrepo-com%201.png'
-                    firstP.innerHTML = item
-                    secondP.innerHTML = myValues[index]
-                    secondP.style.color = '#FFE713';
-                    myEle.appendChild(firstImg)
-                    myEle.appendChild(firstP)
-                    myEle.appendChild(secondP)
-                    mainDiv.appendChild(myEle)
-                })
+                createMainTitle()
+                createCards(keyValues)
             }
         })
         .catch((e) => console.log(e.message))
