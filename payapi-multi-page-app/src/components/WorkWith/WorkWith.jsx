51db012457d8ac7d7cc699e03487848ad3ae8fd0
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-04-09 23:59:10 -0300

    feature/work-with components+scss

---
+++ payapi-multi-page-app/src/components/WorkWith/WorkWith.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import teslaIcon from '../../assets/shared/desktop/tesla.svg'
+import microsoftIcon from '../../assets/shared/desktop/microsoft.svg'
+import hewlettIcon from '../../assets/shared/desktop/hewlett-packard.svg'
+import oracleIcon from '../../assets/shared/desktop/oracle.svg'
+import googleIcon from '../../assets/shared/desktop/google.svg'
+import nvidiaIcon from '../../assets/shared/desktop/nvidia.svg'
+
+export const WorkWith = () => {
+    return (
+        <section className="section-workwith">
+            <div className="wrapper workwith-container">
+                <div className="about-partners">
+                    <h2>Who we work with</h2>
+                    <p>Today, millions of people around the world have successfully connected
+                        their accounts to apps they love using our API. We provide developers
+                        with the tools they need to create easy and accessible experiences
+                        for their users.</p>
+                    <button>About Us</button>
+                </div>
+                <div className="partners-icons">
+                    <img src={teslaIcon} alt="tesla-logo"/>
+                    <img src={microsoftIcon} alt="microsoft-logo"/>
+                    <img src={hewlettIcon} alt="hewlett-logo"/>
+                    <img src={oracleIcon} alt="oracle-logo"/>
+                    <img src={googleIcon} alt="google-logo"/>
+                    <img src={nvidiaIcon} alt="nvidia-logo"/>
+                </div>
+            </div>
+        </section>
+    )
+}
