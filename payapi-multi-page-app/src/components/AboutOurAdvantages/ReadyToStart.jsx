4bde5f134aa233a828ae21520f7e134e66c5a8f0
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-04-12 00:05:33 -0300

    about-advantages component & scss

---
+++ payapi-multi-page-app/src/components/AboutOurAdvantages/ReadyToStart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ReadyToStart(props) {
+    return (
+        <div>
+            <h2>Ready to start?</h2>
+            <input type="text"/>Enter email address
+            <button>Schedule a Demo</button>
+        </div>
+    );
+}
+
+export default ReadyToStart;
